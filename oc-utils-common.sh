OpenCAPI/oc-utils

commit 733c24920a4a6affa0edc1d740342ec6ba1e99c9

reset, reload, utils-common: accept new slot names (#2)
This commit extends oc-reset and oc-reload to accept the new
opencapi slot names, that are platform-dependant.

Platforms that still use the previous OPENCAPI-xxxx standard
for slot names are supported as well.

Signed-off-by: Philippe Bergheaud <felix@linux.ibm.com>

oc-utils-common.sh
@@ -33,6 +33,22 @@ function reset_card() {
     echo 100000 > /sys/kernel/debug/powerpc/eeh_max_freezes
   fi
 
+  # if necessary, convert card name into slot name
+  slot=$1
+  if [[ ! -f /sys/bus/pci/slots/$slot/power ]]
+  then
+    if [[ -c /dev/ocxl/$slot ]]
+    then
+      slot=`printf "$slot\n" | awk -F"." '{ print $2 }' | sed s/$/.0/`
+    fi
+    slot=`lspci -m -v -s $slot | awk '/^PhySlot:/ { print $2; exit }'`
+    if [[ -z $slot ]]
+    then
+      printf "$1: No such card or slot. Exiting.\n"
+      exit 1
+    fi
+  fi
+
   [ -n "$3" ] && printf "$3\n" || printf "Preparing to reset card\n"
   [ -n "$4" ] && reset_timeout=$4
   sleep 5
@@ -42,9 +58,9 @@ function reset_card() {
   sleep 3
   c=$1
   printf "reset card is set to \"$c\". Reset!\n"
-  printf 0 > /sys/bus/pci/slots/$c/power
+  printf 0 > /sys/bus/pci/slots/$slot/power
   sleep 3
-  printf 1 > /sys/bus/pci/slots/$c/power
+  printf 1 > /sys/bus/pci/slots/$slot/power
   sleep 5
   while true; do
     if [[ `ls -d /sys/class/ocxl/IBM* 2> /dev/null | awk -F"/sys/class/ocxl/" '{ print $2 }' | wc -w` == "$n" ]]; then
@@ -88,19 +104,36 @@ function reload_card() {
     echo 100000 > /sys/kernel/debug/powerpc/eeh_max_freezes
   fi
 
+  # if necessary, convert card name into slot name
+  slot=$1
+  if [[ ! -f /sys/bus/pci/slots/$slot/power ]]
+  then
+    if [[ -c /dev/ocxl/$slot ]]
+    then
+      slot=`printf "$slot\n" | awk -F"." '{ print $2 }' | sed s/$/.0/`
+    fi
+    slot=`lspci -m -v -s $slot | awk '/^PhySlot:/ { print $2; exit }'`
+    if [[ -z $slot ]]
+    then
+      printf "$1: No such card or slot. Exiting.\n"
+      exit 1
+    fi
+  fi
+
   [ -n "$3" ] && printf "$3\n" || printf "Preparing to reset card\n"
   [ -n "$4" ] && reset_timeout=$4
   sleep 5
   modprobe pnv-php
 # added by collin for image_reload
-  setpci -s ${1:9:4}:00:00.0 638.B=01
+# tuned for the new slot naming scheme
+  setpci -s `cat /sys/bus/pci/slots/$slot/address`.0 638.B=01
   printf "Resetting card $1: Image Reloading ... \n"
   sleep 3
   c=$1
   printf "image reload is set to \"$c\".\nReset Card...\nReload Image...\n"
-  printf 0 > /sys/bus/pci/slots/$c/power
+  printf 0 > /sys/bus/pci/slots/$slot/power
   sleep 3
-  printf 1 > /sys/bus/pci/slots/$c/power
+  printf 1 > /sys/bus/pci/slots/$slot/power
   sleep 5
   while true; do
     if [[ `ls -d /sys/class/ocxl/IBM* 2> /dev/null | awk -F"/sys/class/ocxl/" '{ print $2 }' | wc -w` == "$n" ]]; then

oc-flash-script.sh
@@ -155,7 +155,7 @@ while read d ; do
       flash_block[$i]=${parse_info[4]}
       flash_interface[$i]=${parse_info[5]}
       flash_secondary[$i]=${parse_info[6]}
-      printf "%-20s %-30s %-29s %-20s %s\n" "card$i:OPENCAPI-${allcards_array[$i]:0:4}" "${line:6:21}" "${f:0:29}" "${f:30:20}" "${f:51}"
+      printf "%-20s %-30s %-29s %-20s %s\n" "card$i:${allcards_array[$i]}" "${line:6:21}" "${f:0:29}" "${f:30:20}" "${f:51}"
     fi
   done < "$package_root/oc-devices"
   i=$[$i+1]
@@ -305,7 +305,7 @@ wait $PID
 RC=$?
 #if [ $RC -eq 0 ]; then
 #  # reset card only if Flashing was good, TBD
-#  printf "Test infomration ${allcards_array[$c]:0:4}"
-#   ./oc-reset.sh -C OPENCAPI-${allcards_array[$c]:0:4}
+#  printf "Test information ${allcards_array[$c]"
+#   ./oc-reset.sh -C ${allcards_array[$c]}
 #fi
 

oc-list-cards.sh
@@ -24,7 +24,7 @@ while read d ; do
       flash_block[$i]=${parse_info[4]}
       flash_interface[$i]=${parse_info[5]}
       flash_secondary[$i]=${parse_info[6]}
-      printf "%-20s %-30s %-29s %-20s %s\n" "card$i:OPENCAPI-${allcards_array[$i]:0:4}" "${line:6:21}" "${f:0:29}" "${f:30:20}" "${f:51}"
+      printf "%-20s %-30s %-29s %-20s %s\n" "card$i:${allcards_array[$i]}" "${line:6:21}" "${f:0:29}" "${f:30:20}" "${f:51}"
     fi
   done < "$package_root/oc-devices"
   i=$[$i+1]

oc-reload.sh
@@ -25,11 +25,13 @@ program=`basename "$0"`
 # Print usage message helper function
 function usage() {
   echo "Usage:  sudo ${program} [OPTIONS]"
-  echo "    [-C <card>] card to reset. You need to Specify bdf device number!"
-  echo "      Example: if you want to reset card" 
+  echo "    [-C <card>] card to reload."
+  echo "      Example: if you want to reload card"
   echo -e "        \033[33m IBM,oc-snap.0004:00:00.1.0 \033[0m"
   echo "      Command line should be:"
-  echo -e "        \033[33m sudo ./oc-reset.sh -C OPENCAPI-0004 \033[0m"
+  echo -e "        \033[33m sudo ./oc-reload.sh -C IBM,oc-snap.0004:00:00.1.0 \033[0m"
+  echo "      Or:"
+  echo -e "        \033[33m sudo ./oc-reload.sh -C 0004:00:00.0 \033[0m"
   echo "    [-V] Print program version (${version})"
   echo "    [-h] Print this help message."
   echo
@@ -120,8 +122,6 @@ if [ -n "$card" ]; then
 else
         select_cards
         # Find all OC cards in the system
-	allcards=`ls -d /sys/class/ocxl/IBM* | awk -F"/sys/class/ocxl/" '{ print $2 }' |sed s/IBM,MEMCPY3./OPENCAPI-/g  |sed s/IBM,AFP3./OPENCAPI-/g |sed s/IBM,oc-snap./OPENCAPI-/g | awk -F":" '{print $1}' | sort`
-        allcards_array=($allcards)
         n=`ls -d /sys/class/ocxl/IBM* | awk -F"/sys/class/ocxl/" '{ print $2 }' | wc -w`
 	if (($c < 0 )) || (( "$c" >= "$n" )); then
             printf "${bold}ERROR:${normal} Wrong card number ${c}\n"

oc-reset.sh
@@ -25,11 +25,13 @@ program=`basename "$0"`
 # Print usage message helper function
 function usage() {
   echo "Usage:  sudo ${program} [OPTIONS]"
-  echo "    [-C <card>] card to reset. You need to Specify bdf device number!"
+  echo "    [-C <card>] card to reset."
   echo "      Example: if you want to reset card" 
   echo -e "        \033[33m IBM,oc-snap.0004:00:00.1.0 \033[0m"
   echo "      Command line should be:"
-  echo -e "        \033[33m sudo ./oc-reset.sh -C OPENCAPI-0004 \033[0m"
+  echo -e "        \033[33m sudo ./oc-reset.sh -C IBM,oc-snap.0004:00:00.1.0 \033[0m"
+  echo "      Or:"
+  echo -e "        \033[33m sudo ./oc-reset.sh -C 0004:00:00.0 \033[0m"
   echo "    [-V] Print program version (${version})"
   echo "    [-h] Print this help message."
   echo
@@ -121,8 +123,6 @@ if [ -n "$card" ]; then
 else
         select_cards
         # Find all OC cards in the system
-	allcards=`ls -d /sys/class/ocxl/IBM* | awk -F"/sys/class/ocxl/" '{ print $2 }' |sed s/IBM,MEMCPY3./OPENCAPI-/g  |sed s/IBM,AFP3./OPENCAPI-/g |sed s/IBM,oc-snap./OPENCAPI-/g | awk -F":" '{print $1}' | sort`
-        allcards_array=($allcards)
         n=`ls -d /sys/class/ocxl/IBM* | awk -F"/sys/class/ocxl/" '{ print $2 }' | wc -w`
 	if (($c < 0 )) || (( "$c" >= "$n" )); then
             printf "${bold}ERROR:${normal} Wrong card number ${c}\n"

commit f900e0019dddd1bfd2fb1f69a5a169a293bb5b77

utils-common: load module pnv-php before accessing physical slot
This patch fixes a regression introduced by commit 733c249.

Signed-off-by: Philippe Bergheaud <felix@linux.ibm.com>

oc-utils-common.sh
@@ -34,6 +34,7 @@ function reset_card() {
   fi
 
   # if necessary, convert card name into slot name
+  modprobe pnv-php	# required to access physical slot
   slot=$1
   if [[ ! -f /sys/bus/pci/slots/$slot/power ]]
   then
@@ -52,8 +53,6 @@ function reset_card() {
   [ -n "$3" ] && printf "$3\n" || printf "Preparing to reset card\n"
   [ -n "$4" ] && reset_timeout=$4
   sleep 5
-  modprobe pnv-php
-
   printf "Resetting card $1: "
   sleep 3
   c=$1
@@ -105,6 +104,7 @@ function reload_card() {
   fi
 
   # if necessary, convert card name into slot name
+  modprobe pnv-php	# required to access physical slot
   slot=$1
   if [[ ! -f /sys/bus/pci/slots/$slot/power ]]
   then
@@ -123,7 +123,6 @@ function reload_card() {
   [ -n "$3" ] && printf "$3\n" || printf "Preparing to reset card\n"
   [ -n "$4" ] && reset_timeout=$4
   sleep 5
-  modprobe pnv-php
 # added by collin for image_reload
 # tuned for the new slot naming scheme
   setpci -s `cat /sys/bus/pci/slots/$slot/address`.0 638.B=01